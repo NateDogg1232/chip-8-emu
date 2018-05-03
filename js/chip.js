7554ed971430c8f5ca22973ee2627e609a936d5e
NateDogg1232 <nathanielbarragan@gmail.com>
2018-05-02 22:36:31 -0700

    Added a lot

--- js/chip.js
+++ js/chip.js
@@ -8,6 +8,21 @@ var init = false;
 
 var program;
 
+var ram;
+
+var regs;
+
+//Specifically the I register
+var ir;
+
+//Instruction pointer
+var ip;
+
+//Stack pointer
+var sp;
+
+var stack;
+
 var screenbuffer;
 
 //Takes two arguments: Drawfunct, which is the function that the library will use to draw
@@ -24,20 +39,68 @@ function chipInit(drawfunct, errorhand) {
 	screenbuffer = new Array(64);
 	for (let i = 0; i < 64; i++)
 		screenbuffer[i] = new Array(32);
+	ram = new Array(4096);
+	stack = new Array(16);
+	sp = 0;
+	regs = new Array(16);
+	//Start of
+	ip = 512;
 }
 
 //Load a program. Takes a hex string
 function loadProgram(programStr) {
 	program = new Array(0);
-	for (let i = 0; i < programStr.length / 6; i++) {
+	for (let i = 0; i < programStr.length / 2; i++) {
 		//Convert the hex code to the program
 		//Anything at the end of it must be padded by zeroes
-		program.push(parseInt(programStr.substr(i * 6, 6), 16));
+		//512 is the start of most programs
+		ram[i+512] = parseInt(programStr.substr(i * 2, 2), 16));
 	}
 	resetComputer();
 }
 
+function tick() {
+	//Start running through the program
+	//This variable is for ease of use. Writing out the whole thing takes too darn long
+	var o = ram[ip] | ram[ip + 1];
+	if (o == 0xE0) {
+		//CLS 0xE0		CLS
+		clearScreen();
+		ip+=2;
+	} else if (o == 0xEE) {
+		//RET 0xEE		RET
+		ip = pop();
+	} else if (o & 0x1000 == 0x1000) {
+		//JP 0x1nnn		JP addr
+		ip = o & 0xFFF;
+	} else if (o & 0x2000 == 0x2000) {
+		//CALL 0x2nnn	CALL addr
+		push(ip);
+		ip = 0 & 0xFF;
+	} else if (o & 0x3000 == 0x3000) {
+		//SE 0x3xkk		SE Vx, byte
+	}
+}
+
+function push(value) {
+	stack[sp] = value;
+	sp++;
+	if (sp > 15) {
+		//TODO: Stack overflow
+	}
+}
+
+function pop() {
+	let ret = stack[sp];
+	sp--;
+	if (sp < 0) {
+		//TODO: Stack underflow
+	}
+}
+
 //Resets the computer to the default state
 function resetComputer() {
-
+	screenbuffer = new Array(64);
+	for (let i = 0; i < 64; i++)
+		screenbuffer[i] = new Array(32);
 }
